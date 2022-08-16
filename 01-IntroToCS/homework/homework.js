aa5b0e8db9675d0aa4e1eef339c8b9491eee6eef
Matthew Gallar <matthewgallar2112@outlook.com>
2022-08-16 16:50:41 -0300

    IntroDone v0.1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,47 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var array = [];
+  var resultado = 0;
+  num = num.split("");
 
+  var numInvert = num.reverse();
+
+  for (let i=0; i<numInvert.length; i++){
+    var res = numInvert[i]* Math.pow(2,i);
+    
+    array.push(res);
+  }
+  for(let i=0; i<array.length;i++){
+    resultado += array[i];
+     
+  }
+  return resultado;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+ 
+  parseInt(num);
 
+  var array = [];
+  
+
+  for (let i=0; i<num+5; i++){
+  
+      
+    if(num>0){
+        
+      array.unshift(num%2);
+      num = Math.floor(num/2)
+      
+    }
+     else if(num === 0){
+       break;
+    }
+    
+  }
+  return array.join("");
 }
 
 
@@ -15,3 +50,4 @@ module.exports = {
   BinarioADecimal,
   DecimalABinario,
 }
+  
