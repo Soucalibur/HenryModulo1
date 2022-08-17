60ee67e04252f87be995b545bb205f329629d1d9
Matthew Gallar <matthewgallar2112@outlook.com>
2022-08-17 10:08:23 -0300

    Intro done v0.3

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,7 +1,18 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  //tu codigo aca
+  
+  //Codigos del profe
+  //function BinarioADecimal(num){
+  //  let resultado = 0;
+  //  num = num.split("").reverse();
+  //  for(let i=0; i<num.length;i++){
+  //    resultado = resultado + num[i]*2**i;
+  //  }
+  //  return resultado;
+  //}
+
   var array = [];
   var resultado = 0;
   num = num.split("");
@@ -20,26 +31,31 @@ function BinarioADecimal(num) {
   return resultado;
 }
 
+
 function DecimalABinario(num) {
   // tu codigo aca
- 
+ //Codigo del profe
+ //function DecimalABinario(num){
+ // let binary = [];
+ // while (num <0){
+ //   let resto= num%2;
+ //   binary = unshift(rest);
+ //   num = Math.floor(num/2);
+ // }
+ // return binary.join("");
+ //}
   parseInt(num);
 
   var array = [];
-  
 
   do{
     
       array.unshift(num%2);
       num = Math.floor(num/2)
-      
-     
+         
   }
   while(num!==0);
       
-    
-    
-  
   return array.join("");
 }
 
