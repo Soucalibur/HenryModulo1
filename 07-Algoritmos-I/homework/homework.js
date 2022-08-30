0606a728202bd9a5dba4fb705da8a818d93cd422
Matthew Gallar <matthewgallar2112@outlook.com>
2022-08-30 08:34:20 -0300

    AI v0.1

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,8 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+
+
 }
 
 function bubbleSort(array) {
@@ -15,8 +17,25 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-}
+  for(let i=0;i<array.length;i++){
+    for(let x=0;x<array.length;x++){
+      var uno = 0
+      var dos = 0
+      
+      if(array[x]>array[x+1]){
+        let uno = array[x];
+        let dos = array[x+1];
+        array[x] = dos;
+        array[x+1] = uno; 
+      }
 
+    } 
+    
+  }
+  return array;
+
+}
+console.log(bubbleSort([3,2,5,7,1]))
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
@@ -24,8 +43,24 @@ function insertionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-}
+  for(let i=0;i<array.length;i++){
+    for(let i=0;i<array.length;i++){
+      var uno = 0
+      var dos = 0
 
+      if(array[i]<array[i-1]){
+        let uno = array[i];
+        let dos = array[i-1];
+        array[i] = dos;
+        array[i-1] = uno; 
+      }
+      
+    } 
+  }
+  
+  return array;
+}
+console.log(insertionSort([3,2,5,7,1]))
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
